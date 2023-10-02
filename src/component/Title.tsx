e59bc3e79c1dd67be1ab3d7be764d2234700299a
도진경 <ehwlsrud1@naver.com>
2023-10-02 17:24:27 +0900

    feat:타이틀 추가 및 css 수정

---
+++ src/component/Title.tsx
@@ -0,0 +1,6 @@
+import styles from "./title.module.scss";
+
+const Title = () => {
+  return <h1 className={styles.title}>채널리스트 작업툴</h1>;
+};
+export default Title;
--- src/component/tabBar.tsx
+++ src/component/tabBar.tsx
@@ -1,27 +1,31 @@
 import { NavLink } from "react-router-dom";
 import styles from "./tabBar.module.scss";
 import classNames from "classnames";
+import Title from "./Title";
 
 const cx = classNames.bind(styles);
 const TabBar = () => {
   return (
-    <div className={styles.nav}>
-      <NavLink
-        className={({ isActive }) =>
-          cx(styles.navBtn, { [styles.isActive]: isActive })
-        }
-        to="/"
-      >
-        사용 방법
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          cx(styles.navBtn, { [styles.isActive]: isActive })
-        }
-        to="/converting"
-      >
-        추출하기
-      </NavLink>
+    <div className={styles.page}>
+      <Title />
+      <div className={styles.nav}>
+        <NavLink
+          className={({ isActive }) =>
+            cx(styles.navBtn, { [styles.isActive]: isActive })
+          }
+          to="/"
+        >
+          사용 방법
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            cx(styles.navBtn, { [styles.isActive]: isActive })
+          }
+          to="/converting"
+        >
+          추출하기
+        </NavLink>
+      </div>
     </div>
   );
 };
